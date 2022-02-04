8d6c3805cf61191cd6af4505e1758e82b270c9c7
satyam-0 <71664719+satyam-0@users.noreply.github.com>
2022-02-04 19:06:51 +0530

    modified

--- nodemailertest.js
+++ nodemailertest.js
@@ -1,11 +1,13 @@
-
+// this file is just for testing the mail 
+// it insure that ... sending mail is properly worked or not. 
 
  const nodemailer = require('nodemailer');
  let mailTransporter = nodemailer.createTransport({
      service: 'hotmail',
      auth: {
          user: 'satyam-0@outlook.com',
-         pass: 'Enter your password '
+         pass: 'Enter your password
+	     '
      }
  });
  let mailDetails = {
